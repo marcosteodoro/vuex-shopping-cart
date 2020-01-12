6d47a1e90cf0a50122d9b834801e5e5a0fe1b7e4
Marcos Teodoro <marcosvinici_us@live.com>
2020-01-12 11:20:19 -0300

    Initial Commit

---
+++ src/store/index.js
@@ -0,0 +1,66 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import shop from '@/api/shop';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    products: [],
+    cart: []
+  },
+
+  getters: {
+    availableProducts (state, getters) {
+      return state.products.filter(product => product.inventory > 0);
+    }
+  },
+
+  actions: {
+    fetchProducts ({commit}) {
+      return new Promise((resolve, reject) => {
+        shop.getProducts (products => {
+          commit('setProducts', products)
+          resolve();
+        })
+      })
+    },
+
+    addProductToCart (context, product) {
+      if (product.inventory > 0) {
+        const cartItem = context.state.cart.find(item => item.id === product.id);
+
+        if (!cartItem) {
+          context.commit('pushProductToCart', product.id);
+        } else {
+          context.commit('incrementItemQuantity', cartItem)
+        }
+
+        context.commit('decrementProductInventory', product);
+      }
+    }
+  },
+
+  mutations: {
+    setProducts (state, products) {
+      state.products = products
+    },
+
+    pushProductToCart (state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      })
+    },
+
+    incrementItemQuantity (state, cartItem) {
+      cartItem.quantity++;
+    },
+
+    decrementProductInventory (state, product) {
+      product.inventory--;
+    }
+  }
+});
+
+
