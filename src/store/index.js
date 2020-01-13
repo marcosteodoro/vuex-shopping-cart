876df1d6929a0067bb6827f897b68749b46ecef5
Marcos Teodoro <marcosvinici_us@live.com>
2020-01-13 00:58:13 -0300

    Remove commented imports

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,5 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
-// import actions from './actions';
-// import getters from './getters';
-// import mutations from './mutations';
-// import state from './state';
 import cart from './modules/cart';
 import products from './modules/products';
 
