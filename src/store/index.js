281ca847352ddb4c7f30fd5cf1b764bb6c869ec2
Marcos Teodoro <marcosvinici_us@live.com>
2020-01-12 23:27:01 -0300

    Implement checkout actions in store

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     products: [],
-    cart: []
+    cart: [],
+    checkoutStatus: null
   },
 
   getters: {
@@ -49,11 +50,24 @@ export default new Vuex.Store({
         if (!cartItem) {
           context.commit('pushProductToCart', product.id);
         } else {
-          context.commit('incrementItemQuantity', cartItem)
+          context.commit('incrementItemQuantity', cartItem);
         }
 
         context.commit('decrementProductInventory', product);
       }
+    },
+
+    checkout ({ state, commit }) {
+      shop.buyProducts(
+        state.cart,
+        () => {
+          commit('emptyCart');
+          commit('setCheckoutStatus', 'success');
+        },
+        () => {
+          commit('setCheckoutStatus', 'fail');
+        }
+      )
     }
   },
 
@@ -75,6 +89,14 @@ export default new Vuex.Store({
 
     decrementProductInventory (state, product) {
       product.inventory--;
+    },
+
+    setCheckoutStatus (state, status) {
+      state.checkoutStatus = status;
+    },
+
+    emptyCart (state) {
+      state.cart = [];
     }
   }
 });
