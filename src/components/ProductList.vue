68ac11f2f8f027ceee12a2964121805be69c59e5
Marcos Teodoro <marcosvinici_us@live.com>
2020-01-13 00:16:39 -0300

    Dinamic mapping vuex store with spread operator

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -19,6 +19,8 @@
 
 <script>
 
+import { mapState, mapGetters, mapActions } from 'vuex';
+
 export default {
   data () {
     return {
@@ -27,24 +29,26 @@ export default {
   },
 
   computed: {
-    products () {
-      return this.$store.state.products;
-    },
+    ...mapState({
+      products: state => state.products
+    }),
 
-    productIsInStock () {
-      return this.$store.getters.productIsInStock
-    }
+    ...mapGetters({
+      productIsInStock: 'productIsInStock'
+    })
   },
 
   methods: {
-    addProductToCart (product) {
-      this.$store.dispatch('addProductToCart', product);
-    }
+    ...mapActions({
+      fetchProducts: 'fetchProducts',
+      addProductToCart: 'addProductToCart',
+
+    })
   },
 
   created() {
     this.loading = true;
-    this.$store.dispatch('fetchProducts')
+    this.fetchProducts()
       .then(() => this.loading = false);
   }
 }
